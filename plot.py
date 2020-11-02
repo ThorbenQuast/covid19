3a7012d4d5a9504631d03bce22c5f2a59d794bad
Thorben  Quast <thorbenquast@MacBook-Pro-3.local>
2020-11-02 22:45:09 +0100

    show deaths per inhabitant rate

--- plot.py
+++ plot.py
@@ -18,6 +18,14 @@ restriction_dates = {
 	"Germany": datetime.date(2020, 3, 22),
 	"UK": datetime.date(2020, 3, 24),
 }
+population = {
+	"Switzerland": 8.6e6,
+	"Italy": 60.3e6,
+	"France": 67.1e6,
+	"Germany": 83e6,
+	"UK": 66.7e6,
+	"USA": 328.2e6
+}
 DAYOFFSET = 31
 ref_date = datetime.date(2020, 1, 22)
 colors = {
@@ -102,43 +110,42 @@ for max_day in all_days:
 			days.append(day-DAYOFFSET)
 			try:
 				infected.append(np.array(data_infected_byCountry[country][day])[0])
-				deaths.append(np.array(data_deaths_byCountry[country][day])[0])
+				deaths.append(np.array(data_deaths_byCountry[country][day])[0]*100000./population[country])
 				recovered.append(np.array(data_recovered_byCountry[country][day])[0])
 			except:
 				import pdb
 				pdb.set_trace()
 
 
-		N_infected = max(infected)
-		infected = 100.*np.array(infected)/N_infected
-		deaths = 100.*np.array(deaths)/N_infected
-		recovered = 100.*np.array(recovered)/N_infected
+		N_infected = max(deaths)
+		#infected = 100.*np.array(infected)/N_infected
+		deaths = np.array(deaths)
+		#recovered = 100.*np.array(recovered)/N_infected
 
-		plt.plot(days, infected, color=colors[country], linewidth=2, linestyle="solid")
-		plt.plot(days, deaths, color=colors[country], linewidth=2, linestyle="dashed")
-		plt.plot(days, recovered, color=colors[country], linewidth=2, linestyle="dotted")
+		#plt.plot(days, infected, color=colors[country], linewidth=2, linestyle="solid")
+		plt.plot(days, deaths, color=colors[country], linewidth=3, linestyle="solid", label="%s: %.1f" % (country, max(deaths)))
+		#plt.plot(days, recovered, color=colors[country], linewidth=2, linestyle="dotted")
 		plt.text((drawindex%3)*0.33*(max_day-DAYOFFSET+1), 115-5.*(drawindex/3), "%s, %i infected"%(country, N_infected), color=colors[country], family="monospace", fontsize=10)
 		updated_date = ref_date+datetime.timedelta(days=DAYOFFSET)+datetime.timedelta(days=max(days))
 		updated_date.strftime("%d/%m/%Y")
-		plt.text(0, -10, updated_date)
+		plt.text(0, -10, "Last update: "+str(updated_date), fontsize=14)
 
+	'''
 	#plot travel restriction / shutdown lines
 	for country in restriction_dates:
 		if restriction_dates[country]<=max_day:
 			plt.axvline(restriction_dates[country]-0.1, ymin=-0., ymax=0.05, color=colors[country], linewidth=1, linestyle="solid")
 			plt.axvline(restriction_dates[country], ymin=-0., ymax=0.05, color=colors[country], linewidth=1, linestyle="solid")
 			plt.axvline(restriction_dates[country]+0.1, ymin=-0., ymax=0.05, color=colors[country], linewidth=1, linestyle="solid")
+	'''
 
-	plt.text((max_day-DAYOFFSET+1)*1.03, 40., "Source:", rotation=90, family="monospace", fontsize=10, color="black")
+	plt.text((max_day-DAYOFFSET+1)*1.03, 40., "Data taken from:", rotation=90, family="monospace", fontsize=12, color="black")
 	plt.text((max_day-DAYOFFSET+1)*1.06, 0., "https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases", rotation=90, family="monospace", fontsize=10, color="black")
-	plt.plot(days, np.zeros(len(days)), color="black", linestyle="solid", label="infected")
-	plt.plot(days, np.zeros(len(days)), color="black", linestyle="dashed", label="dead")
-	plt.plot(days, np.zeros(len(days)), color="black", linestyle="dotted", label="recovered")
-	plt.legend(loc="upper left")
+	plt.legend(loc="upper left", fontsize=16)
 
 	plt.xlim(0., max_day-DAYOFFSET+1)
-	plt.xlabel("#Days after 22 Feb 2020")
-	plt.ylabel("Fraction of total infected in country/region [x100]")
+	plt.xlabel("#Days after 22 Feb 2020", fontsize=18)
+	plt.ylabel("Number of Covid-19-related deaths / 100,000 inhabitants", fontsize=18)
 	plot_list.append(plt_path)
 	print("Saving",plt_path)
 	plt.savefig(plt_path)
